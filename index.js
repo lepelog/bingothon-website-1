ab14166feb81f6099c523fc0fc4edec02d4b2a88
Chris C <chrissico@outlook.de>
2019-01-02 23:39:46 +0100

    Fix favicon not displaying

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var fs = require("fs");
+var favicon = require("serve-favicon");
 
 const express = require("express");
 const app = express();
 const port = 8091;
+
+app.use(favicon("www/favicon.ico"));
 
 app.get("/", function(req, res) {
   var index = fs.readFileSync("www/index.html", "utf-8");
