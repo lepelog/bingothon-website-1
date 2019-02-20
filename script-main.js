7eb525d5eb2aa320cc49334210943e84a339a94c
oliver <o-lohr@gmx.de>
2019-02-20 11:41:00 +0100

    Dowloaded external dependencies, got rid of javascript

--- script-main.js
+++ script-main.js
@@ -1,13 +1,13 @@
 window.onload=function(){
 
   //loading
-  $(".menu-item").click(function() {
+  /*$(".menu-item").click(function() {
 	window.history.pushState('obj', 'Submissions', '/staff');
     $("body").load("staff.html");
         return false;
-  });
-	$(".menu-item").animate({"opacity": "1"}, 1000);
-	$(".menu-item").hover(function(){
+  });*/
+	/*$(".menu-item").animate({"opacity": "1"}, 1000);*/
+	/*$(".menu-item").hover(function(){
 		$(this).filter(':not(:animated)').animate({ letterSpacing: "1px" });
 	}, function() {
 		$(this).animate({ letterSpacing: "0px" });
@@ -41,5 +41,5 @@ window.onload=function(){
 		"backgroundColor": "#1e1e1e",
 		"color": "#d2303f"
 	  });
-	});
+	});*/
 }
